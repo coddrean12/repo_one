38724ed8562b1ee5c559d6dd53de5aea247ff511
coddrean12 <coddrean12@gmail.com>
2024-11-16 03:37:23 -0800

    atm

--- atm_app.py
+++ atm_app.py
@@ -1,5 +1,6 @@
-account = (input ("Please input your bank numbe: "))
+account = (input ("Please input your bank number: "))
 password = (input ("Please input your password: "))
 amount = (input ("How much do you want to withdrawl: "))
 amount = (input ("How much do you want to withdrawl: "))
 print(f"Withdrawl a success you \n You were able to withdraw {amount}")
+print("help")
